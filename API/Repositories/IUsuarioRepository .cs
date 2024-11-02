027c1169387e1f94a8a4f2c00fc2b3ddaa4051c0
Eriberto Lima <ter.eriberto.lima@sada.com.br>
2024-11-02 13:52:49 -0300

    feat: create users repository interface

---
+++ API/Repositories/IUsuarioRepository .cs
@@ -0,0 +1,10 @@
+﻿using API.Models;
+
+namespace API.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        Task<Usuario> AdicionarUsuarioAsync(Usuario usuario);
+        Task<bool> UsuarioExisteAsync(string email);
+    }
+}
